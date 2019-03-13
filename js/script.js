eb44c2032bbdd589a56e52a58c175da53471d66e
sylviavdb <47741532+sylviavdb@users.noreply.github.com>
2019-03-13 10:26:44 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,28 +80,7 @@ window.onload = function () {
 
 // begin datavisual2//
 
-	var donutGrafiek = new CanvasJS.Chart("chartContainer2", {
-		animationEnabled: true,
-		title:{
-			text: "Travellers",
-			horizontalAlign: "center",
-			fontFamily: "Arial",
-		},
-		data: [{
-			type: "doughnut",
-			startAngle: 60,
-			//innerRadius: 60,
-			indexLabelFontSize: 12,
-			indexLabel: "{label} - #percent%",
-			toolTipContent: "<b>{label}:</b> {y} (#percent%)",
-			dataPoints: [
-				{ y: 53, label: "Women" },
-				{ y: 40, label: "Men", color:"#003333" },
-				{ y: 10, label: "Children", color: "darkgrey" }
-			]
-		}]
-	});
-	donutGrafiek.render();
+	
 
 	//begin datavisual3
 
