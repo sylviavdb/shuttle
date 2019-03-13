d4d1e3a2c2346fdc0fdeab23a6e386f2e2c7e174
sylviavdb <47741532+sylviavdb@users.noreply.github.com>
2019-03-13 10:28:51 +0100

    Update and rename script.js to datavisual1.js

--- js/datavisual1.js
+++ js/datavisual1.js
@@ -77,44 +77,5 @@ window.onload = function () {
 		staafDiagram.render();
 	}
 
-
-// begin datavisual2//
-
-	
-
-	//begin datavisual3
-
-	var lijnGrafiek = new CanvasJS.Chart("chartContainer3", {
-		animationEnabled: true,
-		theme: "light2",
-		title:{
-			text: "Temperature shuttle",
-			fontFamily: "Arial",
-		},
-		axisY:{
-			includeZero: false
-		}, 
-		data: [{        
-			type: "line",  
-			fontFamily: "Arial",     
-			dataPoints: [
-				{ y: 20 },
-				{ y: 25},
-				{ y: 29, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
-				{ y: 27 },
-				{ y: 27 },
-				{ y: 28 },
-				{ y: 26 },
-				{ y: 26 },
-				{ y: 19 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-				{ y: 22 },
-				{ y: 22 },
-				{ y: 21 }
-			]
-		}]
-	});
-	lijnGrafiek.render();
-
-
 }
 
