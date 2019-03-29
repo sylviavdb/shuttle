9fa392f8126a6dff12f2ff92876c2a2a0a015afb
sylviavdb <47741532+sylviavdb@users.noreply.github.com>
2019-03-29 22:09:27 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ window.onload = function () {
 
 // end datavisual 1
 // start datavisual 2
-window.onload = function(){
+
 var donutGrafiek = new CanvasJS.Chart("chartContainer2", {
 		animationEnabled: true,
 		title:{
@@ -106,7 +106,39 @@ var donutGrafiek = new CanvasJS.Chart("chartContainer2", {
 		}]
 	});
 	donutGrafiek.render();
-}
+
 // end datavisual2
 // start datavisual3
+
+var lijnGrafiek = new CanvasJS.Chart("chartContainer3", {
+		animationEnabled: true,
+		theme: "light2",
+		title:{
+			text: "Temperature shuttle",
+			fontFamily: "Arial",
+		},
+		axisY:{
+			includeZero: false
+		}, 
+		data: [{        
+			type: "line",  
+			fontFamily: "Arial",     
+			dataPoints: [
+				{ y: 20 },
+				{ y: 25},
+				{ y: 29, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
+				{ y: 27 },
+				{ y: 27 },
+				{ y: 28 },
+				{ y: 26 },
+				{ y: 26 },
+				{ y: 19 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
+				{ y: 22 },
+				{ y: 22 },
+				{ y: 21 }
+			]
+		}]
+	});
+	lijnGrafiek.render();
+  
 
